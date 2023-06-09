f0a338e16541b060be6eae037c8aa4f2e8f02cb5
trodriguez10 <95036319+trodriguez10@users.noreply.github.com>
2023-06-08 21:34:07 -0300

    LetElim primera version

--- LetElim.hs
+++ LetElim.hs
@@ -16,7 +16,23 @@ import Data.List
 -- ELIMINACION DE LETs
 
 letElimP :: Program -> Program
-letElimP (Program defs expr) = undefined
+letElimP (Program defs expr) = Program (map letElimDef defs) (letElimExpr expr)
 
+letElimDef :: FunDef -> FunDef
+letElimDef (FunDef typedFun names expr) = FunDef typedFun names (letElimExpr expr)
+
+letElimExpr :: Expr -> Expr
+letElimExpr (Var name) = (Var name)  -- capaz llevar a expr al fondo y devolver expr nomas => PATTERN MATCHING
+letElimExpr expr@(IntLit _) = expr -- Dos de las sintaxis validas para esta parte
+letElimExpr expr@(BoolLit _) = expr
+letElimExpr (Infix op expr expr') = Infix op (letElimExpr expr) (letElimExpr expr')
+letElimExpr (If condExpr expr expr') = If (letElimExpr condExpr) (letElimExpr expr) (letElimExpr expr')
+letElimExpr (Let (name, _) expr expr') =
+  let substExpr = subst name expr expr' -- Chequear Free variables
+  in letElimExpr substExpr -- lo puse asi por que seguramente haya que hacer algo mas, si no, nos podemos ahorrar el let in
+letElimExpr (App name args) = App name (map letElimExpr args)
+
+
+-- CHEQUEAR LO DE VARIABLES LIGADAS
 subst :: Name -> Expr -> Expr -> Expr
-subst name subExpr expr = undefined
+subst = undefined

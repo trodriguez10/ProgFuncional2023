19c6360598442c09ade3e08c1e678c67e9fd9065
William Tift <william.tift@decemberlabs.com>
2023-06-10 20:46:54 -0300

    Tested parte 3 with ejemplos de la letra

--- LetElim.hs
+++ LetElim.hs
@@ -54,3 +54,43 @@ subst name expr (App name' args) = App name' (map (subst name expr) args)
 subst _ _ expr' = expr'
 
 
+
+
+
+
+-- TESTS DE CHEQUEOS
+
+-- let x = 3 in x + 2 * x
+program1 :: Program
+program1 = Program [] (Let ("x", TyInt) (IntLit 3) (Infix Add (Var "x") (Infix Mult (IntLit 2) (Var "x"))))
+
+-- let x = 3 * 4 in x + 2
+program2 :: Program
+program2 = Program [] (Let ("x", TyInt) (Infix Mult (IntLit 3) (IntLit 4)) (Infix Add (Var "x") (IntLit 2)))
+
+-- let x = 3 in let x = 4 in x + 2
+program3 :: Program
+program3 = Program [] (Let ("x", TyInt) (IntLit 3) (Let ("x", TyInt) (IntLit 4) (Infix Add (Var "x") (IntLit 2))))
+
+-- let x = 3 in let y = x in y + 2
+program4 :: Program
+program4 = Program [] (Let ("x", TyInt) (IntLit 3) (Let ("y", TyInt) (Var "x") (Infix Add (Var "y") (IntLit 2))))
+
+-- let x = 3 in let x = 4 + x in x + 2
+program5 :: Program
+program5 = Program [] (Let ("x", TyInt) (IntLit 3) (Let ("x", TyInt) (Infix Add (IntLit 4) (Var "x")) (Infix Add (Var "x") (IntLit 2))))
+
+-- let x = (let y = 4 in y) in x + 2
+program6 :: Program
+program6 = Program [] (Let ("x", TyInt) (Let ("y", TyInt) (IntLit 4) (Var "y")) (Infix Add (Var "x") (IntLit 2)))
+
+-- let x = 5 + (let y = 4 in y) in x + 2
+program7 :: Program
+program7 = Program [] (Let ("x", TyInt) (Infix Add (IntLit 5) (Let ("y", TyInt) (IntLit 4) (Var "y"))) (Infix Add (Var "x") (IntLit 2)))
+
+-- let x = (let y = 4 in y + 3) in x + 2
+program8 :: Program
+program8 = Program [] (Let ("x", TyInt) (Let ("y", TyInt) (IntLit 4) (Infix Add (Var "y") (IntLit 3))) (Infix Add (Var "x") (IntLit 2)))
+
+testCheckProgram :: Program -> IO ()
+testCheckProgram p = putStrLn (show (letElimP p))
